35b6f9e4624b9c929b3891f02f8e5ed633c30bed
Carlos Ramos <carlos.dg147@gmail.com>
2025-06-21 01:31:27 -0400

    Refactorizando render de las tareas.

--- js/app.js
+++ js/app.js
@@ -54,9 +54,6 @@ class Task {
 
 // * Variables Globales
 var allTasks = [];
-var divListTasksPending = document.getElementById("listTasksPending");
-var divListTaskDone = document.getElementById("listTasksDone");
-var divListDeletedTasks = document.getElementById("listDeletedTasks");
 
 
 /** Llenado inicial de las tareas */
@@ -110,6 +107,8 @@ function eventEditTask(task, pTask) {
 function eventRestoreFromTrash(task, aRestoreTask) {
   aRestoreTask.addEventListener('click', function(e) {
     e.preventDefault();
+    e.stopPropagation();
+
     undeleteTask(task);
     refreshData();
     showAlertMsg("Tarea restaurada.");
@@ -119,6 +118,7 @@ function eventRestoreFromTrash(task, aRestoreTask) {
 function eventDeleteTaskPermanently(task, btnDeletePermanent) {
   btnDeletePermanent.addEventListener('click', function (e) {
     e.preventDefault();
+    e.stopPropagation();
 
     if (confirm("¿Estás seguro de que deseas eliminar esta tarea permanentemente?")) {
       deleteTaskPermanently(task);
@@ -372,87 +372,109 @@ function loadTasks() {
  * View
  */
 function refreshData() {
-  divListTasksPending.innerHTML = "";
-  divListTaskDone.innerHTML = "";
-  divListDeletedTasks.innerHTML = "";
-  for(let task of allTasks) {
-    printTask(task);
-  }
+  renderPendingTasks();
+  renderCompletedTasks();
+  renderDeletedTasks();
+
   updateTextCounterTaskPending();
   updatePercentageTaskDone();
 }
 
-function printDeletedTasks(task) {
-  var articleTask = document.createElement('article');
-  articleTask.classList.add('task');
-  articleTask.id = task.id;
+function renderPendingTasks() {
+  var divListTasksPending = document.getElementById("listTasksPending");
+  divListTasksPending.innerHTML = "";
 
-  var pTask = document.createElement('p');
-  pTask.innerText = task.description;
+  allTasks.filter(task => !task.done && !task.delete).forEach(task => {
+    var articleTask = document.createElement('article');
+    articleTask.classList.add('task');
+    articleTask.id = task.id;
 
-  var aRestoreTask = document.createElement('a');
-  aRestoreTask.href = "#";
-  aRestoreTask.innerText = "Restaurar";
-
-  var btnDeletePermanent = document.createElement("a");
-  btnDeletePermanent.href = "#";
-  btnDeletePermanent.classList.add("color-primary-m-1", "btn-delete-permanent");
-  btnDeletePermanent.innerText = "Eliminar";
-
-  articleTask.appendChild(pTask);
-  articleTask.appendChild(aRestoreTask);
-  articleTask.appendChild(btnDeletePermanent);
-  divListDeletedTasks.appendChild(articleTask);
-
-  eventRestoreFromTrash(task, aRestoreTask);
-  eventDeleteTaskPermanently(task, btnDeletePermanent)
-}
-
-function printTask(task) {
-  if(task.delete) {
-    printDeletedTasks(task);
-    return;
-  }
-  var articleTask = document.createElement('article');
-  articleTask.classList.add('task');
-  articleTask.id = task.id;
-
-  var pTask = document.createElement('p');
-  pTask.innerText = task.description;
-  if(task.done) {
-    pTask.classList.add('completed');
-  } else {
+    var pTask = document.createElement('p');
+    pTask.innerText = task.description;
     pTask.setAttribute('contenteditable', '');
-  }
 
-  var btnDoneRestore = document.createElement('button');
-  if(task.done) {
-    btnDoneRestore.classList.add('btn-restore');
-    btnDoneRestore.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#fb713b"><path d="M480-80q-75 0-140.5-28.5t-114-77q-48.5-48.5-77-114T120-440h80q0 117 81.5 198.5T480-160q117 0 198.5-81.5T760-440q0-117-81.5-198.5T480-720h-6l62 62-56 58-160-160 160-160 56 58-62 62h6q75 0 140.5 28.5t114 77q48.5 48.5 77 114T840-440q0 75-28.5 140.5t-77 114q-48.5 48.5-114 77T480-80Z"/></svg>'
-  } else {
+    var btnDoneRestore = document.createElement('button');
     btnDoneRestore.classList.add('btn-done');
     btnDoneRestore.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#fff"><path d="M480-80q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q65 0 123 19t107 53l-58 59q-38-24-81-37.5T480-800q-133 0-226.5 93.5T160-480q0 133 93.5 226.5T480-160q133 0 226.5-93.5T800-480q0-18-2-36t-6-35l65-65q11 32 17 66t6 70q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm-56-216L254-466l56-56 114 114 400-401 56 56-456 457Z"/></svg>'
-  }
-
-  var btnDelete = document.createElement('button');
-  btnDelete.classList.add('btn-delete');
-  btnDelete.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#cd3c04"><path d="M280-120q-33 0-56.5-23.5T200-200v-520h-40v-80h200v-40h240v40h200v80h-40v520q0 33-23.5 56.5T680-120H280Zm400-600H280v520h400v-520ZM360-280h80v-360h-80v360Zm160 0h80v-360h-80v360ZM280-720v520-520Z"/></svg>'
-  
-  articleTask.appendChild(pTask);
-  articleTask.appendChild(btnDoneRestore);
-  articleTask.appendChild(btnDelete);
-
-  if(task.done) {
-    divListTaskDone.appendChild(articleTask);
-  } else {
+    
+    var btnDelete = document.createElement('button');
+    btnDelete.classList.add('btn-delete');
+    btnDelete.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#cd3c04"><path d="M280-120q-33 0-56.5-23.5T200-200v-520h-40v-80h200v-40h240v40h200v80h-40v520q0 33-23.5 56.5T680-120H280Zm400-600H280v520h400v-520ZM360-280h80v-360h-80v360Zm160 0h80v-360h-80v360ZM280-720v520-520Z"/></svg>'
+    
+    articleTask.appendChild(pTask);
+    articleTask.appendChild(btnDoneRestore);
+    articleTask.appendChild(btnDelete);
     divListTasksPending.appendChild(articleTask);
-  }
 
-  eventDoneOrRestoreTask(task, btnDoneRestore, articleTask);
-  eventDeleteTask(task, btnDelete);
-  eventEditTask(task, pTask);
-
+    eventDoneOrRestoreTask(task, btnDoneRestore, articleTask);
+    eventDeleteTask(task, btnDelete);
+    eventEditTask(task, pTask);  
+  });
 }
+
+function renderCompletedTasks() {
+  var divListTaskDone = document.getElementById("listTasksDone");
+  divListTaskDone.innerHTML = "";
+
+  allTasks.filter(task => task.done && !task.delete).forEach(task => {
+    var articleTask = document.createElement('article');
+    articleTask.classList.add('task');
+    articleTask.id = task.id;
+
+    var pTask = document.createElement('p');
+    pTask.innerText = task.description;
+    pTask.classList.add('completed');
+
+    var btnDoneRestore = document.createElement('button');
+    btnDoneRestore.classList.add('btn-restore');
+    btnDoneRestore.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#fb713b"><path d="M480-80q-75 0-140.5-28.5t-114-77q-48.5-48.5-77-114T120-440h80q0 117 81.5 198.5T480-160q117 0 198.5-81.5T760-440q0-117-81.5-198.5T480-720h-6l62 62-56 58-160-160 160-160 56 58-62 62h6q75 0 140.5 28.5t114 77q48.5 48.5 77 114T840-440q0 75-28.5 140.5t-77 114q-48.5 48.5-114 77T480-80Z"/></svg>'
+
+    var btnDelete = document.createElement('button');
+    btnDelete.classList.add('btn-delete');
+    btnDelete.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#cd3c04"><path d="M280-120q-33 0-56.5-23.5T200-200v-520h-40v-80h200v-40h240v40h200v80h-40v520q0 33-23.5 56.5T680-120H280Zm400-600H280v520h400v-520ZM360-280h80v-360h-80v360Zm160 0h80v-360h-80v360ZM280-720v520-520Z"/></svg>'
+    
+    articleTask.appendChild(pTask);
+    articleTask.appendChild(btnDoneRestore);
+    articleTask.appendChild(btnDelete);
+    divListTaskDone.appendChild(articleTask);
+
+    eventDoneOrRestoreTask(task, btnDoneRestore, articleTask);
+    eventDeleteTask(task, btnDelete);
+  })
+}
+
+function renderDeletedTasks() {
+  var divListDeletedTasks = document.getElementById("listDeletedTasks");
+  divListDeletedTasks.innerHTML = "";
+
+  allTasks.filter(task => task.delete).forEach(task => {
+    var articleTask = document.createElement('article');
+    articleTask.classList.add('task');
+    articleTask.id = task.id;
+  
+    var pTask = document.createElement('p');
+    pTask.innerText = task.description;
+  
+    var aRestoreTask = document.createElement('a');
+    aRestoreTask.href = "#";
+    aRestoreTask.innerText = "Restaurar";
+  
+    var btnDeletePermanent = document.createElement("a");
+    btnDeletePermanent.href = "#";
+    btnDeletePermanent.classList.add("color-primary-m-1", "btn-delete-permanent");
+    btnDeletePermanent.innerText = "Eliminar";
+  
+    articleTask.appendChild(pTask);
+    articleTask.appendChild(aRestoreTask);
+    articleTask.appendChild(btnDeletePermanent);
+    divListDeletedTasks.appendChild(articleTask);
+  
+    eventRestoreFromTrash(task, aRestoreTask);
+    eventDeleteTaskPermanently(task, btnDeletePermanent)
+  })
+}
+
+
 
 /**
  * Menu
